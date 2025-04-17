f09ca88ed1fc399251262ef30f0818cebf73ea07
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2025-04-17 19:20:42 +0200

    Forgot this one, oops

---
+++ dot_config/doom/packages.el
@@ -0,0 +1,26 @@
+(package! olivetti)
+(package! org-roam )
+(package! ivy-bibtex)
+(package! helm-bibtex)
+(package! org-ref)
+(package! org-roam-bibtex)
+(package! org-super-agenda)
+(package! org-noter)
+
+(package! org-modern)
+(package! org-appear)
+(package! org-glossary
+  :recipe (:host github :repo "tecosaur/org-glossary"))
+(package! org-modern-indent
+  :recipe (:host github :repo "jdtsmith/org-modern-indent"))
+
+(package! focus)
+(package! centered-window)
+
+(package! org-roam-ui)
+(package! org-download)
+
+(package! ox-hugo)
+
+(package! snakemake-mode)
+(package! engrave-faces)
