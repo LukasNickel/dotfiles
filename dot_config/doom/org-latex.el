e20aaf5b3328526a68cb322726377951177187d9
Lukas Nickel <l.nickel@ianus-simulation.de>
2026-04-24 11:27:36 +0200

    remove code for diss, add class for work, remove explicit toolbox latexmk command.

--- dot_config/doom/org-latex.el
+++ dot_config/doom/org-latex.el
@@ -15,7 +15,9 @@
 (setq org-latex-src-block-backend 'engraved)
 
 (setq org-latex-compiler "lualatex")
-(setq org-latex-pdf-process (list "toolbox -c latex run latexmk -f %f -output-directory=%o"))
+                                        ; TODO: This needs to be available from whereever emacs is installed.
+                                        ; If emacs is in a toolbox, install latex there. Otherwise install latex in a distrobox container and use distrobox-export to expose latexmk etc
+(setq org-latex-pdf-process (list "latexmk -f %f -output-directory=%o"))
 
 (defun org-export-output-file-name-modified (orig-fun extension &optional subtreep pub-dir)
   (unless pub-dir
@@ -35,49 +37,10 @@
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
              )
 
-                                        ; https://emacs.stackexchange.com/questions/51328/how-can-i-position-label-outside-caption-in-latex-export-of-figures
-(defun org-latex--caption/label-string (element info)
-  "Return caption and label LaTeX string for ELEMENT.
-
-INFO is a plist holding contextual information.  If there's no
-caption nor label, return the empty string.
-
-For non-floats, see `org-latex--wrap-label'."
-  (let* ((label (org-latex--label element info nil t))
-         (main (org-export-get-caption element))
-         (attr (org-export-read-attribute :attr_latex element))
-         (type (org-element-type element))
-         (nonfloat (or (and (plist-member attr :float)
-                            (not (plist-get attr :float))
-                            main)
-                       (and (eq type 'src-block)
-                            (not (plist-get attr :float))
-                            (null (plist-get info :latex-listings)))))
-         (short (org-export-get-caption element t))
-         (caption-from-attr-latex (plist-get attr :caption)))
-    (cond
-     ((org-string-nw-p caption-from-attr-latex)
-      (concat caption-from-attr-latex "\n"))
-     ((and (not main) (equal label "")) "")
-     ((not main) label)
-     ;; Option caption format with short name.
-     (t
-      (format (if nonfloat "\\captionof{%s}%s{%s}\n%s"
-                "\\caption%s%s{%s}\n%s")
-              (let ((type* (if (eq type 'latex-environment)
-                               (org-latex--environment-type element)
-                             type)))
-                (if nonfloat
-                    (cl-case type*
-                      (paragraph "figure")
-                      (image "figure")
-                      (special-block "figure")
-                      (src-block (if (plist-get info :latex-listings)
-                                     "listing"
-                                   "figure"))
-                      (t (symbol-name type*)))
-                  ""))
-              (if short (format "[%s]" (org-export-data short info)) "")
-              (org-export-data main info)
-              label)))))
+(add-to-list 'org-latex-classes
+             '("ianusarticle"
+               "\\documentclass{ianusarticle}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
 
